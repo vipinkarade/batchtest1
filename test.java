2952bce94f9bd9ece6d551190ab53f1c24094390
vipinkarade <95174101+vipinkarade@users.noreply.github.com>
2022-08-02 19:38:43 +0530

    Update test.java
    from app

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test() {
     public static void main(String[] args){
-        System.out.println("It worked again!");
+        System.out.println("It worked again from app!");
     }
 }
